0cb9267bb3ec2ed60a1e8007148854522acb1def
Steven Li <levensti@gmail.com>
2018-12-09 11:12:17 -0600

    Playing window / canvas.

---
+++ src/Window.java
@@ -0,0 +1,21 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends Canvas {
+    private static final long serialVersionUID = 7883803474888262873L;
+
+    //constructing the window (GUI) for the game to be displayed
+    public Window(int width, int height, String title, Game game) {
+        JFrame frame = new JFrame(title);
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+    }
+}
